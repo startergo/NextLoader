e5946910a876054c56ca20d9e73739abad55ace0
startergo <7897244+startergo@users.noreply.github.com>
2021-01-19 21:44:11 -0500

    NextLoader Initial commit

---
+++ include/version.h
@@ -0,0 +1,14 @@
+/*
+ * include/version.h
+ * Version number header file
+ *
+ * Copyright (c) 2017 Roderick W. Smith
+ *
+ * Distributed under the terms of the GNU General Public License (GPL)
+ * version 3 (GPLv3), a copy of which must be distributed with this
+ * source code or binaries made from it.
+ *
+ */
+
+#define LOADER_VERSION L"1.0.0-b3"
+
